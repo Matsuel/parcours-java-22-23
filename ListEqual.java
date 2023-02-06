6c2886f3f85311e69038d3750c3730b32cd195a1
Mathéo Lang <matheo.lang@ynov.com>
2023-02-06 12:06:24 +0100

    listequal

---
+++ ListEqual.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class ListEqual {
+    public static boolean areListEquals(List<String> list1, List<String> list2) {
+        if(list1.isEmpty()|| list2.isEmpty()){
+            return false;
+        }else{
+            for (int i=0;i<list1.size();i++){
+                String val1= list1.get(i);
+                String val2= list2.get(i);
+                if(val1!=val2){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
