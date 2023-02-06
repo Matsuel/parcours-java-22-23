c0542c41d5148ad3e5e909e52af9cbc857bee4c2
Mathéo Lang <matheo.lang@ynov.com>
2023-02-06 15:25:10 +0100

    formatdate

---
+++ FormatDate.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class FormatDate {
+
+    public static String formatToFullText(LocalDateTime dateTime) {
+        String[] months={"janvier","fevrier","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","decembre"};
+        String date="";
+        date+= "Le "+dateTime.getDayOfMonth()+" "+months[dateTime.getMonthValue()-1]+" de l'an "+dateTime.getYear()+" à "+dateTime.getHour()+"h"+dateTime.getMinute()+"m et "+dateTime.getSecond()+"s";
+        return date;
+    }
+
+    public static String formatSimple(LocalDate date) {
+        String simple="";
+        simple+=date.getMonth()+" "+date.getDayOfMonth()+" "+date.getYear()%100;
+        return simple;
+    }
+
+    public static String formatIso(LocalTime time) {
+        String date="";
+        date+=time.getHour()+":"+time.getMinute()+":"+time.getSecond()+"."+time.getNano();
+        return date;
+    }
+
+}
