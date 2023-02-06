a1eced60495f52f3053f4341afd4941ba844307b
Mathéo Lang <matheo.lang@ynov.com>
2023-02-06 09:55:33 +0100

    stringconcat

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -41,9 +41,17 @@
 // }
 
 
+// public class ExerciseRunner {
+//     public static void main(String[] args) {
+//         System.out.println(StringLength.getStringLength("Hello World !"));
+//         System.out.println(StringLength.getStringLength(""));
+//     }
+// }
+
+
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(StringLength.getStringLength("Hello World !"));
-        System.out.println(StringLength.getStringLength(""));
+        System.out.println(StringConcat.concat("Hello ", "étudiant !"));
+        System.out.println(StringConcat.concat("", "Hello World !"));
     }
 }
