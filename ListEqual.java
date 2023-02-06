411a42b9bb1276f09efcc195806ee110cbff7486
Mathéo Lang <matheo.lang@ynov.com>
2023-02-06 12:09:28 +0100

    listequal

--- ListEqual.java
+++ ListEqual.java
@@ -2,8 +2,8 @@ import java.util.List;
 
 public class ListEqual {
     public static boolean areListEquals(List<String> list1, List<String> list2) {
-        if (list1==null&& list2==null){
-            return false;
+        if (list1==null && list2==null){
+            return true;
         }else if(list1==null|| list2==null){
             return false;
         }else{
