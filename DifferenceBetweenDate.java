8dffc7660f78e11d4c709afc0d090f15f2222b32
Mathéo Lang <matheo.lang@ynov.com>
2023-02-06 15:47:10 +0100

    parsedate

---
+++ DifferenceBetweenDate.java
@@ -0,0 +1,23 @@
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+
+public class DifferenceBetweenDate {
+
+    public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
+        Duration duration= Duration.between(localTime1, localTime2);
+        return duration;
+    }
+
+    public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
+        Period period = Period.between(date1, date2);
+        return period;
+    }
+
+    // public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+    //     Long long = dateTime1.getHour()- dateTime2.getHour();
+    // }
+
+}
