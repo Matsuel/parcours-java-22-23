7aff9e6bdf419adae94531965504adbc442ea4f0
Mathéo Lang <matheo.lang@ynov.com>
2023-02-06 10:47:36 +0100

    computearray

--- ComputeArray.java
+++ ComputeArray.java
@@ -1,15 +1,19 @@
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
-        int[] tab= new int[array.length];
-        for (int i=0;i<array.length;i++ ){
-            if (array[i]%3==0){
-                tab[i]= array[i]*5;
-            }else if ((array[i]%3)==1){
-                tab[i]=array[i]+7;
-            }else {
-                tab[i]=array[i];
+        if (array==null){
+            return array;
+        }else{
+            int[] tab= new int[array.length];
+            for (int i=0;i<array.length;i++ ){
+                if (array[i]%3==0){
+                    tab[i]= array[i]*5;
+                }else if ((array[i]%3)==1){
+                    tab[i]=array[i]+7;
+                }else {
+                    tab[i]=array[i];
+                }
             }
+            return tab;
         }
-        return tab;
     }
 }
