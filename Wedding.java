854a99fe5eb20687ac323ad751390475d577aa6f
Matsuel <matsuel0308@gmail.com>
2023-02-07 16:43:35 +0100

    Wedding

--- Wedding.java
+++ Wedding.java
@@ -9,31 +9,34 @@ public class Wedding {
     public static Map<String, String> createCouple(Set<String> first, Set<String> second) {
         if (first==null)return null;
         if (second==null)return null;
-        Map<String, String> result=new HashMap<>();
-        System.out.println(result);
+        Map<String, String> rep=new HashMap<>();
         List<String> first_list= first.stream().sorted().collect(Collectors.toList());
         List<String> second_list= second.stream().sorted().collect(Collectors.toList());
+        System.out.println(first_list);
+        System.out.println(second_list);
         int maxLen =0;
         if (first_list.size()>second_list.size()){
             maxLen=first_list.size();
         }else{
             maxLen=second_list.size();
         }
+        Random rand = new Random();      
         for (int i=0; i<maxLen;i++){
-            // Random rand1= new Random();
-            // Random rand2= new Random();
-            // int a= rand1.nextInt(0,first_list.size())+0;
-            // int b= rand2.nextInt(0,second_list.size())+0;
+            if (first_list.size()>1){
+                int a= rand.nextInt(maxLen);
+                int b= rand.nextInt(maxLen);
+                String first_el= first_list.get(a);
+                String second_el= second_list.get(b);
+                first_list.remove(first_el);
+                second_list.remove(second_el);
+                rep.put(first_el, second_el);
+                maxLen--;
 
+                
+            }
+            rep.put(first_list.get(0), second_list.get(0));
             
-            int a = (int) Math.floor(Math.random() *(first_list.size() - 0 ) + 0);
-            int b = (int) Math.floor(Math.random() *(second_list.size() - 0 ) + 0);
-            //int b = (int)Math.random()*(second_list.size()-0);
-            result.putIfAbsent(first_list.get(a), second_list.get(b));
-            
-
-            System.out.println(first_list.get(a)+"-"+second_list.get(b));
         }
-        return result;
+        return rep;
     }
 }
