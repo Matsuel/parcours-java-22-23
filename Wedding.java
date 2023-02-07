399903a698a3be9ef88da0c009a952fb605a9a0d
Matsuel <matsuel0308@gmail.com>
2023-02-07 16:58:02 +0100

    Wedding

--- Wedding.java
+++ Wedding.java
@@ -21,8 +21,9 @@ public class Wedding {
             maxLen=second_list.size();
         }
         Random rand = new Random();      
+        System.out.println(maxLen);
         for (int i=0; i<maxLen;i++){
-            if (first_list.size()>1){
+            if (second_list.size()>1){
                 int a= rand.nextInt(maxLen);
                 int b= rand.nextInt(maxLen);
                 String first_el= first_list.get(a);
@@ -30,11 +31,19 @@ public class Wedding {
                 first_list.remove(first_el);
                 second_list.remove(second_el);
                 rep.put(first_el, second_el);
+                maxLen--;                
+            }else if (first_list.size()>=1){
+                System.out.println("cc");
+                int a= rand.nextInt(maxLen);
+                String first_el= first_list.get(a);
+                rep.put(first_el, null);
+                first_list.remove(first_el);
                 maxLen--;
-
-                
             }
+            System.out.println(first_list.size());
             rep.put(first_list.get(0), second_list.get(0));
+            first_list.remove(first_list.get(0));
+            second_list.remove(second_list.get(0));
             
         }
         return rep;
