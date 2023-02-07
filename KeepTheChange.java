964ad6cc64f6b810bb413291ab8c81e3b6264316
Matsuel <matsuel0308@gmail.com>
2023-02-07 14:51:06 +0100

    keepthechange

--- KeepTheChange.java
+++ KeepTheChange.java
@@ -1,25 +1,22 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class KeepTheChange {
     public static List<Integer> computeChange(int amount, Set<Integer> coins) {
         if (amount==0) return new ArrayList<>();
         if (coins==null) return null;
+        List<Integer> order_coins = coins.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        System.out.println(order_coins);
         List<Integer> result = new ArrayList<>();
-        for (int coin : coins) {
-            if (coin>amount) continue;
-            if (coin==amount) {
+        for(int coin: order_coins){
+            while (amount-coin>=0){
                 result.add(coin);
-                return result;
-            }
-            List<Integer> subResult = computeChange(amount-coin, coins);
-            if (subResult!=null) {
-                result.add(coin);
-                result.addAll(subResult);
-                return result;
+                amount=amount-coin;
             }
         }
-        return null;
+        return result;
     }
 }
