ed700d45448633ad76faad8a54a01eb2c1c8f6f9
Mathéo Lang <matheo.lang@ynov.com>
2023-02-07 20:02:34 +0100

    StarGetters

--- CelestialObject.java
+++ CelestialObject.java
@@ -2,6 +2,7 @@ public class CelestialObject {
     public double x,y,z = 0.0;
     public String name;
 
+    //Constructeur par défaut
     public CelestialObject() {
         this.name = "Soleil";
         this.x = 0.0f;
@@ -9,10 +10,42 @@ public class CelestialObject {
         this.z = 0.0f;
     }
 
+    //Constructeur qui prend en paramètre le nom de l'objet et ses coordonnées x,y,z
     public CelestialObject(String name, double x, double y, double z) {
         this.name = name;
         this.x = x;
         this.y = y;
         this.z = z;
     }
+
+    //*getter pour x,y,z et pour name
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getZ() {
+        return z;
+    }
+    public String getName(){
+        return name;
+    }
+
+    //Setter pour x,y,z et name
+
+    public void setX(double x){
+        this.x=x;
+    }
+
+    public void setY(double y){
+        this.y=y;
+    }
+    public void setZ(double z){
+        this.z=z;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
 }
+
