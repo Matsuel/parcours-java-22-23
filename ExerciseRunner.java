e2094b3a44e86b743c5cc93435243373c1bb4e49
Matsuel <matsuel0308@gmail.com>
2023-02-08 17:46:57 +0100

    WeddingComplex

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -182,14 +182,26 @@
 // }
 
 
-import java.util.Set;
+// import java.util.Set;
+
+// public class ExerciseRunner {
+
+//     public static void main(String[] args) {
+//         System.out.println(Wedding.createCouple(Set.of("Pikachu", "Dracaufeu", "Tortank"), Set.of("Legolas", "Aragorn", "Gimli")));
+//         System.out.println(Wedding.createCouple(Set.of("Pikachu", "Dracaufeu", "Tortank","jack"), Set.of("Legolas", "Aragorn", "Gimli")));
+//         System.out.println(Wedding.createCouple(Set.of("Pikachu", "Dracaufeu", "Tortank","jack","erik"), Set.of("Legolas", "Aragorn", "Gimli")));
+//     }
+// }
+
+import java.util.List;
+import java.util.Map;
 
 public class ExerciseRunner {
 
     public static void main(String[] args) {
-        System.out.println(Wedding.createCouple(Set.of("Pikachu", "Dracaufeu", "Tortank"), Set.of("Legolas", "Aragorn", "Gimli")));
-        System.out.println(Wedding.createCouple(Set.of("Pikachu", "Dracaufeu", "Tortank","jack"), Set.of("Legolas", "Aragorn", "Gimli")));
-        System.out.println(Wedding.createCouple(Set.of("Pikachu", "Dracaufeu", "Tortank","jack","erik"), Set.of("Legolas", "Aragorn", "Gimli")));
+        System.out.println(WeddingComplex.createBestCouple(
+                Map.of("Naruto", List.of("Sakura", "Hinata"), "Sasuke", List.of("Sakura", "Hinata")),
+                Map.of("Sakura", List.of("Sasuke", "Naruto"), "Hinata", List.of("Naruto", "Sasuke"))));
     }
 }
 
---
+++ WeddingComplex.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.Map;
+
+import javax.lang.model.element.Name;
+
+import java.util.HashMap;
+
+public class WeddingComplex {
+    public static Map<String, String> createBestCouple(Map<String, List<String>> first, Map<String, List<String>> second) {
+        Map<String,String> rep=new HashMap<>();
+        for (String name: first.keySet()){
+            System.out.println(name);
+            for(String values : first.get(name)){
+                for (int i=0; i<name.length();i++){
+                    if (name.charAt(i)== values.charAt(i)){
+                        rep.put(name, values);
+                    }
+                }
+            }
+        }
+        return rep;
+    }
+}
