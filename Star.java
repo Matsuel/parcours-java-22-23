b94780c0bc82d426792423d96a3362ec01fce098
Matsuel <matsuel0308@gmail.com>
2023-02-08 16:16:35 +0100

    StarOverride

--- Star.java
+++ Star.java
@@ -30,6 +30,6 @@ public class Star extends CelestialObject{
     }
 
     public int hashCode(){
-        return (int)Math.round((this.getName().hashCode()+this.getX()*this.getMagnitude()+this.getY()*this.getMagnitude()+this.getZ()*this.getMagnitude())*KM_IN_ONE_AU);
+        return (int)Math.round(this.getName().hashCode()+this.getX()*KM_IN_ONE_AU+this.getY()*KM_IN_ONE_AU+this.getZ()*KM_IN_ONE_AU+this.magnitude*KM_IN_ONE_AU);
     }
 }
