af7ce2ec085c2aaa2f68a47735a77d14c0655c34
Matsuel <matsuel0308@gmail.com>
2023-02-08 16:06:52 +0100

    StarOverride

--- Star.java
+++ Star.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Star extends CelestialObject{
     private double magnitude=0.0;
 
@@ -7,5 +9,23 @@ public class Star extends CelestialObject{
 
     public void setMagnitude(double magnitude){
         this.magnitude=magnitude;
+    }
+
+    public Star(){
+        this.magnitude=0.0f;
+        CelestialObject Star= new CelestialObject();
+    }
+
+    public Star(String name, double x, double y, double z, double magnitude) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.magnitude=magnitude;
+    }
+
+    public String toString(){
+        DecimalFormat val= new DecimalFormat("0.000");
+        return this.getName()+" is shines at the "+ val.format(this.getMagnitude());
     }
 }
