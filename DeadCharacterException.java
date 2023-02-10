cb28c3a06c91b74be1c6cf627d02971355a98095
Mathéo Lang <matheo.lang@ynov.com>
2023-02-10 10:16:53 +0100

    AdventureException

--- DeadCharacterException.java
+++ DeadCharacterException.java
@@ -1,6 +1,6 @@
 public class DeadCharacterException extends Exception {
     DeadCharacterException(Character ch){
-        super("The " + ch.getClass().getSimpleName().toLowerCase() + " " + ch.getName() + " is dead.");
+        super("The "+ch.getClass().getSimpleName().toLowerCase()+" "+ch.getName() + " is dead");
     }
 
     public String getMessage(){
