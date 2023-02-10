714e0b9edfe5a18dd56fab4a1e729132a47ccc3e
Mathéo Lang <matheo.lang@ynov.com>
2023-02-10 10:43:26 +0100

    AdventureException

--- DeadCharacterException.java
+++ DeadCharacterException.java
@@ -1,8 +1,19 @@
 public class DeadCharacterException extends Exception {
+    public Character ch;
     DeadCharacterException(Character ch){
+        super();
+        this.ch=ch;
     }
 
     public String getMessage(){
-        return super.getMessage();
+        String rep= "The";
+        if (this.ch instanceof Templar){
+            rep+=" templar ";
+        }else if (this.ch instanceof Sorcerer){
+            rep += " sorcerer ";
+        }else{
+            rep+= " monster ";
+        }
+        return rep+this.ch.getName()+" is dead.";
     }
 }
